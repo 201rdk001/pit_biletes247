82313beae73f8165164f5ee64789eaf87d323e15
Keython <keita.laimina@gmail.com>
2024-11-19 20:03:18 +0200

    profilsfrontend

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -7,7 +7,6 @@ function showWindow(windowId) {
 
 // Function to close the window
 function closeWindow(windowId) {
-    console.log(`Closing window: ${windowId}`);
     const window = document.getElementById(windowId);
     window.classList.add('hidden'); // Hide the window by adding the 'hidden' class
 }
@@ -19,12 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
         showWindow('userProfile');
     });
 
-    // Open ticket purchase window when ticket button is clicked
+    // Open tickets window when ticket button is clicked
     document.getElementById('ticketBtn').addEventListener('click', function () {
         showWindow('createTickets');
     });
 
-    // Close user profile window when close button is clicked
+    // Close window when close button is clicked
     document.querySelectorAll('.closeBtn').forEach(button => {
         button.addEventListener('click', function () {
             const window = button.closest('.window');
