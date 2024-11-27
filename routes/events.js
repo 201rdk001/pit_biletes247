4a1dd04c88032acc9dc56a9ccbcc19f0f77c7bfc
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2024-11-27 13:22:24 +0200

    Add events organizer view

--- routes/events.js
+++ routes/events.js
@@ -1,25 +1,41 @@
 var express = require('express');
 var asyncHn = require('express-async-handler');
-var { loadUpcomingEvents } = require("../models/event");
+var { loadUpcomingEvents, loadOrganizerEvents  } = require("../models/event");
 var router = express.Router();
 router.use(require('../auth'));
 
 /* GET event list page. */
 router.get('/', asyncHn(async function(req, res, next) {
-    const events = await loadUpcomingEvents(req.app.db);
-    events.forEach(event => {
-        const eventTime = new Date(event.event_time * 1000);
-        event.event_time = eventTime.toLocaleString("lv-LV", {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-            weekday: "long"
+    let events;
+    
+    if (req.query.organizer_events && req.session.user.is_organizer) {
+        events = await loadOrganizerEvents(req.app.db, req.session.user.id);
+        events.forEach(event => {
+            event.event_id = event.id;
         });
-        event.price = event.price.toFixed(2);
-    });
+        res.locals.organizer_view = "checked";
+    }
+    else {
+        events = await loadUpcomingEvents(req.app.db);
+        events.forEach(event => {
+            const eventTime = new Date(event.event_time * 1000);
+            event.event_time = eventTime.toLocaleString("lv-LV", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+                weekday: "long"
+            });
+            event.price = event.price.toFixed(2);
+        });
+    }
 
+    if (req.query.search) {
+        events = events.filter(event => event.name.includes(req.query.search));
+    }
+    
+    res.locals.is_organizer = req.session.user.is_organizer;
     res.render('events', { events });
 }));
 
